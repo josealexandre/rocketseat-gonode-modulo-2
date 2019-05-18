781d808f4106f7d109aa8d6faaf121520d676997
Alexandre Junior <jalex.jr@gmail.com>
2019-05-18 00:44:43 -0300

    Configuring server, routes file and sequelize

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'pg',
+    host: '127.0.0.1',
+    username: 'gonode',
+    password: 'gonode',
+    database: 'gonodemodulo2',
+    operatorAliases: false,
+    define: {
+        timestamps: true
+    }
+}
