37efad9876f2fc4d9301a9007adf39491b0673df
Alexandre Junior <jasj_jr@hotmail.com>
2019-05-21 23:58:18 -0300

    Creating signup functionality and storing user with a encrypted password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,11 +1,26 @@
+const bcrypt = require('bcryptjs')
+
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        avatar: DataTypes.STRING,
-        passwordHash: DataTypes.STRING,
-        provider: DataTypes.BOOLEAN
-    })
+    const User = sequelize.define(
+        'User',
+        {
+            name: DataTypes.STRING,
+            email: DataTypes.STRING,
+            avatar: DataTypes.STRING,
+            password: DataTypes.VIRTUAL,
+            passwordHash: DataTypes.STRING,
+            provider: DataTypes.BOOLEAN
+        },
+        {
+            hooks: {
+                beforeCreate: async user => {
+                    if (user.password) {
+                        user.passwordHash = await bcrypt.hash(user.password, 8)
+                    }
+                }
+            }
+        }
+    )
 
     return User
 }
