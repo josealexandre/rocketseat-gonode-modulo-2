6e58a59d1d01a805c0659b5cb89007f8b91e431e
Alexandre Junior <jasj_jr@hotmail.com>
2019-05-26 21:30:23 -0300

    Correcting date column

--- src/database/migrations/20190525233240-create-appointments.js
+++ src/database/migrations/20190525233240-create-appointments.js
@@ -9,7 +9,7 @@ module.exports = {
                 primaryKey: true,
                 type: Sequelize.INTEGER
             },
-            data: {
+            date: {
                 allowNull: false,
                 type: Sequelize.DATE
             },
